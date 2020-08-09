6d623ce1ce4467376a965aa5629098cdfa56b469
vyom <68771081+vyom-32@users.noreply.github.com>
2020-08-09 12:12:50 +0530

    Add files via upload

---
+++ LAB_2/addition.cs
@@ -0,0 +1,10 @@
+namespace calc
+{
+	public class addition
+	{
+		public static int Add(int n1,int n2)
+		{
+			return n1+n2;
+		}
+	}
+}
---
+++ LAB_2/multiplication.cs
@@ -0,0 +1,10 @@
+namespace calc
+{
+	public class multiplication
+	{
+		public static int multiply(int n1,int n2)
+		{
+			return n1*n2;
+		}
+	}
+}
